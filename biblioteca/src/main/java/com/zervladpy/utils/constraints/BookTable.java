13fc64b9a71ea8d4df32eab0150c3c359452b70e
zervladpy <zernovvladislav7@gmail.com>
2023-12-09 17:28:19 +0000

    -a `BookTable` Book table constraints

--- biblioteca/src/main/java/com/zervladpy/utils/constraints/BookTable.java
+++ biblioteca/src/main/java/com/zervladpy/utils/constraints/BookTable.java
@@ -7,7 +7,7 @@ public class BookTable {
     public static final String ID = "idBook";
     public static final String ISBN = "isbn";
     public static final String TITLE = "titulo";
-    public static final String AUTHOR = "author";
+    public static final String AUTHOR = "autor";
     public static final String YEAR = "anho";
     public static final String AVALIABLE = "disponible";
     public static final String COVER = "portada";
